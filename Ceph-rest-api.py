9a9f69a775a6b9da97d269f9aa152207742e92f2
syklinux <syklinux@163.com>
2018-09-18 15:25:50 +0800

    更新

--- Ceph-rest-api.py
+++ Ceph-rest-api.py
@@ -28,14 +28,6 @@ class CephAPI(object):
 			content = requests.put(url,params=params)
 		return content.text
 
-	def Get_Ceph_Version(self):
-		'''
-		ceph 版本信息
-		'''
-		obj = "version"
-		content = self.postRequest(obj,methond='GET')
-		return content
-
 	def Get_Ceph_Status(self):
 		'''
 		ceph 集群的状态
@@ -45,6 +37,15 @@ class CephAPI(object):
 		content = self.postRequest(obj,methond='GET')
 		return content
 
+	def Get_Ceph_Version(self):
+		'''
+		ceph 版本信息
+		'''
+		obj = "version"
+		content = self.postRequest(obj,methond='GET')
+		return content
+
+
 	def Get_Auth_List(self):
 		'''
 		列出所有认证信息
@@ -52,6 +53,39 @@ class CephAPI(object):
 		obj = "auth/list"
 		content = self.postRequest(obj,methond='GET')
 		return content
+
+
+	def Add_auth(self,entity,caps=None):
+		'''
+		为特定实体增加认证信息,如有则返回已有的认证信息
+		caps为指定密钥，为空则随机生成
+		'''
+		obj = "auth/get-or-create"
+		if caps:
+			params = {'entity':entity,'caps':caps}
+		else:
+			params = {'entity':entity}
+		content = self.postRequest(obj,params=params)
+		return content
+
+	def Delete_Auth(self,entity):
+		'''
+		删除指定的实体的认证信息
+		'''
+		obj = "auth/del"
+		params = {'entity':entity}
+		content = self.postRequest(obj,params=params)
+		return "{} has delete successed".format(entity)
+
+	def Update_Auth(self,entity,caps):
+		'''
+		更新指定实体的caps信息
+		'''
+		obj = "auth/caps"
+		params = {'entity':entity,'caps':caps}
+		content = self.postRequest(obj,params=params)
+		return content
+
 
 	def Get_Pool_List(self):
 		'''
@@ -81,7 +115,10 @@ class CephAPI(object):
 		return "pool {} has rename {}".format(srcpoolname,destpoolname)
 
 
+
 a = CephAPI('http://10.1.0.229:5000/api/v0.1/')
-print a.Get_Ceph_Version()
+# print a.Get_Ceph_Version()
+# print a.Add_auth(entity='client.test1')
+# print a.Delete_Auth(entity='client.test1')
 # print a.Get_Ceph_Status()
 # print a.Rename_Pool(srcpoolname='test',destpoolname='test2')
